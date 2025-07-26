91828aa0b9917b46730a0f31e9ff3b75f5ad7193
blazeonitch <basohblaise23@gmail.com>
2025-07-26 14:11:21 +0100

    hellyear

--- lua/config/plugins/terminal.lua
+++ lua/config/plugins/terminal.lua
@@ -1,21 +1,25 @@
 return {
-  {
-    "numToStr/FTerm.nvim",
-    config = function()
-      require("FTerm").setup({
-        blend = 10,
-        dimensions = {
-          height = 0.85,
-          width = 0.85,
-          x = 0.5,
-          y = 0.5
-        },
-        border = 'rounded',
-      })
-    end,
-    keys = {
-      { "<A-i>", function() require("FTerm").toggle() end, desc = "Toggle FTerm" },
-    },
-  },
+	{
+		"numToStr/FTerm.nvim",
+		config = function()
+			require("FTerm").setup({
+				blend = 10,
+				dimensions = {
+					height = 0.85,
+					width = 0.85,
+					x = 0.5,
+					y = 0.5
+				},
+				border = 'rounded',
+				theme = {
+					background = '#1E1E2E', -- Catppuccin "base" (dark)
+					foreground = '#CDD6F4', -- Catppuccin "text"
+					border     = '#F5E0DC', -- Catppuccin "rosewater" (accent)
+				},
+			})
+		end,
+		keys = {
+			{ "<A-i>", function() require("FTerm").toggle() end, desc = "Toggle FTerm" },
+		},
+	},
 }
-
