549af65170fe3c8ccf40949c2a012b079a42bc59
blaiseonitch <basohblaise23@gmail.com>
2025-09-17 20:50:26 +0100

    added folding keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -132,7 +132,20 @@ K({"n", "i"}, "<leader>gf", vim.lsp.buf.declaration, opts)             -- Go to 
 K("n", "<leader>R", ":so %<CR>", opts) -- Reload nvim config
 K("i", "<C-BS>", "<C-w>", opts)        --VSCODE like per word deletion
 K("i", "<C-Del>", "<C-o>dw", opt)
+K("n", "<leader>fc", "za", opts) -- toggle code fold for current code block
+
+-- function to toggle folds for entire file
+K("n", "<leader>fa", function ()
+	local foldLevel = vim.fn.foldlevel(".")
+	if foldLevel > 0 and vim.fn.foldclosed(".") == -1 then
+		vim.cmd("normal! zM") -- close all.
+	else
+		vim.cmd("normal! zR") -- open all.
+	end
+end, opts )
 
 --Disabled keys
 K({ 'i', 'n', 'v' }, '<PageUp>', '<Nop>', { silent = true })
 K({ 'i', 'n', 'v' }, '<PageDown>', '<Nop>', { silent = true })
+
+
